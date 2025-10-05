9189a912db9556ff1191aafc9657db0c7d61c5b8
sbx_user1051 <sbx_user1051@169.254.15.177>
2025-10-05 16:26:07 +0000

    Sun Oct 05 16:26:07 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,7 @@ class CheckoutSolution:
     # Specials: 3A for 130, 2B for 45
 
     def checkout(self, skus):
-        totalCost = 0
+        totalCost = {}
 
         prices = {
             'A': 50,
@@ -14,6 +14,23 @@ class CheckoutSolution:
             'D': 15
         }
 
+        offers = {
+            'A' : (3, 130),
+            'B' : (2, 45) 
+        }
+
+        if not isinstance(skus, str):
+            return -1
+        
+        for ch in skus:
+            if ch not in prices:
+                return -1
+            
+        counts = {}
+        for ch in skus:
+            counts[ch] = counts.get(ch, 0) + 1
+
         return totalCost
+
 
 
